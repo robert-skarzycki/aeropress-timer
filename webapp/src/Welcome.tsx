972c97df0e694b5c6b43f79b5816514040c45816
Robert Skarżycki <3417357+robert-skarzycki@users.noreply.github.com>
2019-05-13 09:34:40 +0200

    Simple Welcome child component via router

---
+++ webapp/src/Welcome.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Welcome extends React.Component {
+  public render(): JSX.Element {
+    return (
+      <div>
+        <h1>Aeropress timer</h1>
+        <button>Zaczynamy!</button>
+      </div>
+    );
+  }
+}
