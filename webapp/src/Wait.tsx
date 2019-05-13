a2cde5b25888f595cd449d4864c02e0d6baa4917
Robert Skarżycki <3417357+robert-skarzycki@users.noreply.github.com>
2019-05-13 21:06:53 +0200

    Timer added to Wait step

--- webapp/src/Wait.tsx
+++ webapp/src/Wait.tsx
@@ -1,14 +1,41 @@
 import React from "react";
 import NextButton from "./NextButton";
 
-export default class Wait extends React.Component {
+export default class Wait extends React.Component<{}, { seconds: number }> {
+  private timer?: NodeJS.Timeout;
+
+  constructor(props: any) {
+    super(props);
+
+    this.state = { seconds: 30 };
+  }
+
+  public componentDidMount() {
+    this.timer = setInterval(this.tick, 1000);
+  }
+
   public render(): JSX.Element {
     return (
       <div>
         <h1>Krok 4 - zaczekaj</h1>
-        <div>Zaczekaj X sekund...</div>
+        {this.state.seconds > 0 ? (
+          <div>Zaczekaj {this.state.seconds} sekund...</div>
+        ) : (
+          <div>Idziemy do następnego kroku!</div>
+        )}
         <NextButton caption="Dalej" path="/wash-filter" />
       </div>
     );
   }
+
+  private tick = () => {
+    if (this.state.seconds === 0 && this.timer) {
+      clearInterval(this.timer);
+      return;
+    }
+    this.setState(s => {
+      const seconds = s.seconds - 1;
+      return { seconds };
+    });
+  };
 }
